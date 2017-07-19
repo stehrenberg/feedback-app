dcb30aa8f03d09b677a63101d1882fad22ea9f18
stehrenberg <sehrenbe@hm.edu>
2017-07-19 03:47:23 +0200

    Incuded MomentJS and generate Questionnaire ID from current date.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Moment from 'moment';
+
 import logo from './logo.svg';
 import './App.css';
 import Questionnaire from './compontents/Questionnaire'
 
 class App extends Component {
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-        </div>
-          <div className="App">
-              <h2>Cooperation Feedback Questionnaire</h2>
-              <Questionnaire />
-          </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={ logo } className="App-logo" alt="logo"/>
+                </div>
+                <div className="App">
+                    <h2>Cooperation Feedback Questionnaire</h2>
+                    <Questionnaire id={ this.generateId() } />
+                </div>
+            </div>
+        );
+    }
+
+    generateId() {
+        return Moment().format("YYYYMMDD");
+    }
 }
 
 export default App;
--- src/compontents/Questionnaire.js
+++ src/compontents/Questionnaire.js
@@ -4,12 +4,19 @@ import questions from '../questionTexts'
 
 class Questionnaire extends Component {
 
-    Questionnaire() {
+    constructor(props) {
+        super(props);
         this.state = {
             isAnswered: false,
+            isSaved: false,
         }
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.props.id, this);
+        this.setState({ isAnswered: true, isSaved: true });
+    }
 
     render() {
         const QUESTIONS = questions;
@@ -17,13 +24,11 @@ class Questionnaire extends Component {
         return (
             <div className="Questionnaire">
                 <h2>Goals</h2>
-                <p>
-                    We would like to improve the quality of the services we supply continuously. We need our
+                <p><strong>We would like to improve the quality of the services we supply continuously. We need our
                     customers' help to achieve this,
                     to help us align to his targets give us ideas of what competences to build and where to
-                    improve.
-                </p>
-                <form>
+                        improve.</strong></p>
+                <form action="" method="POST" onSubmit={ this.handleSubmit.bind(this) }>
                     <Question id="understanding" label={ QUESTIONS.UNDERSTANDING } inputType="number" persist="false" />
                     <Question id="cooperation" label={ QUESTIONS.COOPERATION } inputType="number" persist="false" />
                     <Question id="deliveriesAndRoles" label={ QUESTIONS.DELIVERIES_AND_ROLES } persist="true" />
@@ -36,7 +41,7 @@ class Questionnaire extends Component {
                     <Question id="nps" label={ QUESTIONS.NPS } persist="false" />
                     <Question id="tasks" label={ QUESTIONS.TASKS } persist="false" />
                     <Question id="nextMeeting" label={ QUESTIONS.NEXT_MEETING } inputType="date" persist="false"/>
-
+                    <button type="submit">Speichern</button>
                 </form>
             </div>
 
