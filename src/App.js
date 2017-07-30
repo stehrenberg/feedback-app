4d76f6228ba1342104344026cfff11926f79a8e0
stehrenberg <sehrenbe@hm.edu>
2017-07-31 01:04:29 +0200

    Fix component alignment and replace React logo with Mayflower logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Moment from 'moment';
 
-import logo from './logo.svg';
+import logo from './mayflower_logo.png';
 import './App.css';
 import Questionnaire from './compontents/Questionnaire';
 import questions from './questionTexts.json';
@@ -9,14 +9,13 @@ import questions from './questionTexts.json';
 class App extends Component {
 
     render() {
-
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={ logo } className="App-logo" alt="logo"/>
-                </div>
-                <div className="App">
                     <h2>Cooperation Feedback Questionnaire</h2>
+                </div>
+                <div className="App-content">
                     <Questionnaire id={ this.generateId() } questions={ questions.questionTexts } />
                 </div>
             </div>
