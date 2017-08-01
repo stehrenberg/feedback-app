d870f1caa7c13be47a590ca2fec70066d1d67ebc
stehrenberg <sehrenbe@hm.edu>
2017-08-01 16:57:28 +0200

    extract provisioning into separate shellscript, provision and set up MySQL

---
+++ provision.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DBHOST=localhost
+DBNAME=feedback
+DBUSER=dbuser
+DBPASSWD=best4all
+
+echo "\nStart provisioning root stuff...\n"
+
+apt-get update
+apt-get install -y git-core curl apache2 debconf-utils
+
+echo -e "\n--- Setting up MySQL...\n"
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
+
+apt-get install mysql-server -y
