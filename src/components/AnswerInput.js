82d0c3361a5e9555ea1df53fa32b234795048633
stehrenberg <sehrenbe@hm.edu>
2017-08-05 05:28:09 +0200

    Extract creation of correct InputField for a Question's answer into separate functional component

--- src/components/AnswerInput.js
+++ src/components/AnswerInput.js
@@ -1,49 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactStars from 'react-stars';
 import DatePicker from 'material-ui/DatePicker';
 
-class AnswerInput extends Component {
+function AnswerInput(props) {
 
-    render() {
-        let { isReadOnly, name, value, onChange} = this.props;
-        console.log(isReadOnly);
+    const type = props.inputType;
+    let inputField;
+    let optionalProps = {};
+    const isReadOnly = props.isReadOnly;
 
-        let inputField;
-        let optionalProps = {};
-        const isReadOnly = this.props.isReadOnly;
+    switch(type) {
+        case 'number':
+            optionalProps = isReadOnly? { edit: false } : {};
+            inputField = <ReactStars
+                name={ props.name }
+                count={ 10 }
+                value={ props.value }
+                onChange={ (rating) => props.onChange(props.name, rating) }
+                half={ 'false' }
+                size={ 24 }
+                color2={ '#ffc300' }
+                {...optionalProps} />;
+            break;
 
-        switch(type) {
-            case 'number':
-                optionalProps = isReadOnly? { edit: false } : {};
-                inputField = <ReactStars
-                    name={ this.props.name }
-                    count={ 10 }
-                    value={ this.props.value }
-                    onChange={ (rating) => this.props.onChange(this.props.name, rating) }
-                    half={ 'false' }
-                    size={ 24 }
-                    color2={ '#ffc300' }
-                    {...optionalProps} />;
-                break;
-            case 'date':
-                optionalProps = isReadOnly? { disabled: true, hintText: this.props.value } : { hintText: "Pick a date" };
-                inputField = <DatePicker
-                    value={ this.props.value }
-                    {...optionalProps}/>
-                break;
-            default:
-                optionalProps = isReadOnly? { disabled: true } : {};
-                inputField = <input
-                    name={ this.props.name }
-                    type={ this.props.inputType }
-                    placeholder={ this.props.placeholder }
-                    value={ this.props.value }
-                    onChange={ (event) => this.props.onChange(this.props.name, event.target.value) }
-                    {...optionalProps} />;
-                break;
-        }
+        case 'date':
+            optionalProps = isReadOnly? { disabled: true, hintText: props.value } : { hintText: "Pick a date" };
+            const handleChange = (event, date) => props.onChange(props.name, date);
+            inputField = <DatePicker
+                id={ props.name }
+                value={ !(!props.value)? new Date(props.value) : null }
+                onChange={ handleChange }
+                {...optionalProps}/>
+            break;
 
-        return inputField;
+        default:
+            optionalProps = isReadOnly? { disabled: true } : {};
+            inputField = <input
+                name={ props.name }
+                type={ props.inputType }
+                placeholder={ props.placeholder }
+                value={ props.value }
+                onChange={ (event) => props.onChange(props.name, event.target.value) }
+                {...optionalProps} />;
+            break;
     }
 
+    return inputField;
 }
+
+export default AnswerInput;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import ReactStars from 'react-stars';
-import DatePicker from 'material-ui/DatePicker';
+import AnswerInput from '../components/AnswerInput';
 
 class Question extends Component {
 
@@ -8,54 +7,10 @@ class Question extends Component {
         return (
             <div className="Question">
                 <label id={ this.props.id }>{ this.props.label }</label>
-                { this.getAppropriateInputField(this.props.inputType) }
+                <AnswerInput {...this.props}/>
             </div>
         );
     }
-
-    getAppropriateInputField(type) {
-        let inputField;
-        let optionalProps = {};
-        const isReadOnly = this.props.isReadOnly;
-
-        switch(type) {
-            case 'number':
-                optionalProps = isReadOnly? { edit: false } : {};
-                inputField = <ReactStars
-                    name={ this.props.name }
-                    count={ 10 }
-                    value={ this.props.value }
-                    onChange={ (rating) => this.props.onChange(this.props.name, rating) }
-                    half={ 'false' }
-                    size={ 24 }
-                    color2={ '#ffc300' }
-                    {...optionalProps} />;
-                    break;
-
-            case 'date':
-                optionalProps = isReadOnly? { disabled: true, hintText: this.props.value } : { hintText: "Pick a date" };
-                const handleChange = (event, date) => this.props.onChange(this.props.name, date);
-                inputField = <DatePicker
-                    id={ this.props.name }
-                    value={ !(!this.props.value)? new Date(this.props.value) : null }
-                    onChange={ handleChange }
-                    {...optionalProps}/>
-                break;
-
-            default:
-                optionalProps = isReadOnly? { disabled: true } : {};
-                inputField = <input
-                    name={ this.props.name }
-                    type={ this.props.inputType }
-                    placeholder={ this.props.placeholder }
-                    value={ this.props.value }
-                    onChange={ (event) => this.props.onChange(this.props.name, event.target.value) }
-                    {...optionalProps} />;
-                    break;
-        }
-
-        return inputField;
-    }
 }
 
-export default Question
+export default Question;
