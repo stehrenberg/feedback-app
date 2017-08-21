b53a546204f56db21fca9782328e77304608d7c3
stehrenberg <sehrenbe@hm.edu>
2017-08-21 16:29:24 +0200

    Refactor data fetching from API and transform API result into sensible data structure for application

--- src/pages/form-history/FormHistory.js
+++ src/pages/form-history/FormHistory.js
@@ -18,7 +18,7 @@ class FormHistory extends Component {
     }
 
     componentWillMount() {
-        this.fetchFormData().then((formDataAsArray) => this.setState({ forms: formDataAsArray }));
+        this.fetchFormData().then((formDataAsArray) => this.setState({forms: formDataAsArray}));
     }
 
     render() {
@@ -51,33 +51,78 @@ class FormHistory extends Component {
 
     fetchFormData() {
         const surveyResultsEndpoint = `${appConfig.dreamfactoryApi.apiBaseUrl}_table/survey_result?order=survey_id&group=survey_id%2C%20question_id`;
+        const formsData = this.fetchSurveyIdsList().then((surveyIdList) => {
+            const surveyIdsAsArray = surveyIdList;
 
-        return fetch(surveyResultsEndpoint, {
-            method: 'GET',
+            return fetch(surveyResultsEndpoint, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-DreamFactory-Api-Key': appConfig.dreamfactoryApi.apiKey
+                },
+            }).then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    console.log(response.error);
+                }
+            }).then((data) => {
+                const formsData = this.organizeBySurveyId(data.resource, surveyIdList);
+                return formsData;
+            }).catch(err => console.log(err));
+        });
+
+        return formsData;
+    }
+
+    fetchSurveyIdsList() {
+        const surveyIdsEndpoint = `${appConfig.dreamfactoryApi.apiBaseUrl}_table/survey`;
+
+        const transformationFunc = (data) => {
+            const surveyIds = data.resource.map(resource => resource.survey_id);
+            return surveyIds;
+        };
+
+        return this.fetchDataFrom(surveyIdsEndpoint, 'GET', transformationFunc);
+    }
+
+    fetchDataFrom(apiEndpoint, httpMethod, dataTransformMethod) {
+        return fetch(apiEndpoint, {
+            method: httpMethod,
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'X-DreamFactory-Api-Key': appConfig.dreamfactoryApi.apiKey
             },
         }).then((response) => {
-            if(response.ok) {
+            if (response.ok) {
                 return response.json();
             } else {
                 console.log(response.error);
             }
         }).then((data) => {
-            const bla = data.resource.map((resource) => {
-                return {
-                    id: resource.survey_id,
-                    data : resource,
-                };
-            });
-            return bla;
+            return dataTransformMethod(data);
         }).catch(err => console.log(err));
     }
 
+    organizeBySurveyId(rawData, surveyIdList) {
+        return surveyIdList.map((surveyId) => {
+            let newEntry = {
+                surveyId: surveyId,
+                data: rawData.filter((resultTuple) => resultTuple.survey_id === surveyId).map(resultTuple => {
+                    return {
+                        questionId: resultTuple.question_id,
+                        questionValue: resultTuple.question_answer,
+                    };
+                })
+            };
+            return newEntry;
+        });
+    }
+
     findQuestion(form, questionId) {
-        console.log(form, Array.isArray(form));
+        //console.log(form, Array.isArray(form));
         //const question = form.find(data => data.id === questionId);
         return '';//!(!question) ? question.value : '---';
     }
