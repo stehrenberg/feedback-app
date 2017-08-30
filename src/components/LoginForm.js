9c6f2db102fed53a85d9a984d4b0c26cb7cb597b
stehrenberg <sehrenbe@hm.edu>
2017-08-30 18:08:55 +0200

    Specify PropTypes for LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import LogoHeader from './LogoHeader';
 import LoginBtn from './buttons/LoginBtn';
@@ -39,5 +40,9 @@ class LoginForm extends Component {
         );
     }
 }
+
+LoginForm.PropTypes = {
+    history: PropTypes.object.isRequired,
+};
 
 export default LoginForm;
