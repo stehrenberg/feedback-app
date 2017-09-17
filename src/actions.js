d09b90d0c7cc348e108089bcf889db069a121aad
stehrenberg <sehrenbe@hm.edu>
2017-09-18 00:36:03 +0200

    Adding action to set the JWT when logging in

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,17 @@
-export const LOGIN = 'LOGIN';
-
 export const SET_PROJECT = 'SET_PROJECT';
+export const LOGIN = 'LOGIN';
 
 export const setProject = (projectName) => {
     return {
         type: SET_PROJECT,
         projectName,
+    };
+}
+
+export const setJWT = (jwt) => {
+    console.log("setJWT called!");
+    return {
+        type: LOGIN,
+        jwt
     };
 }
