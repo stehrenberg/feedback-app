fbda7c77fd9f19a875e113706cc014e9e306b689
stehrenberg <sehrenbe@hm.edu>
2017-09-18 00:38:42 +0200

    First draft of a PrivateRoute component that needs authentication

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,3 +1,17 @@
-/**
- * Created by praktikum on 15.09.17.
- */
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+    return (<p>bla</p>);
+        /*<Route {...rest} render={ props => (
+        isAuthenticated ?
+        <Component {...props}/> : (
+        <Redirect to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }}/>)
+      )}/>
+    );*/
+};
+
+export default PrivateRoute;
