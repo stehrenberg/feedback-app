60493b3bf502585ac210000e7c6123451de48d3d
stehrenberg <sehrenbe@hm.edu>
2017-09-18 00:35:25 +0200

    Insert reducer function for putting the JWT in the global state

--- src/reducers.js
+++ src/reducers.js
@@ -3,12 +3,22 @@ import './actions';
 
 const feedbackApp = combineReducers({
     projectName,
+    jwt,
 });
 
 function projectName(state = '', action) {
     switch(action.type) {
         case 'SET_PROJECT':
             return action.projectName;
+        default:
+            return state;
+    }
+}
+
+function jwt(state = '', action) {
+    switch(action.type) {
+        case 'LOGIN':
+            return action.jwt;
         default:
             return state;
     }
