00bb5faeee12a496e5fd097fcfe3274caf13c746
stehrenberg <sehrenbe@hm.edu>
2017-09-19 01:24:20 +0200

    Make App a class component instead of a functional one so lifecycle methods can be used

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import createHashHistory from 'history/createHashHistory';
 import PropTypes from 'prop-types';
@@ -11,25 +11,32 @@ import FormDetail from "../components/FormDetail";
 import LoginForm from "../pages/login/LoginForm";
 import AppMenu from '../components/app-menu/AppMenu';
 
-const App = ({ store }) => {
-    const checkAuthentication = () => Object.keys(store.getState().jwt).length > 0;
+class App extends Component {
 
-    return (
-            <MuiThemeProvider>
-                <HashRouter history={ createHashHistory({ queryKey: false }) }>
-                    <Switch>
-                        <PrivateRoute exact path='/' component={ AppMenu } isAuthenticated={ checkAuthentication }/>
-                        <PrivateRoute exact path='/home/:projectName' component={ AppMenu } isAuthenticated={ checkAuthentication }/>
-                        <Redirect from ='/home/:projectName' to='/home' component={ AppMenu } isAuthenticated={ checkAuthentication }/>
-                        <PrivateRoute exact path='/feedback' component={ FeedbackForm } isAuthenticated={ checkAuthentication }/>
-                        <PrivateRoute exact path='/form-history' component={ FormHistory } isAuthenticated={ checkAuthentication }/>
-                        <PrivateRoute path="/feedback/:formId" component={ FormDetail } isAuthenticated={ checkAuthentication }/>
-                        <Route exact path='/login' component={ LoginForm }/>
-                    </Switch>
-                </HashRouter>
-            </MuiThemeProvider>
-    );
-};
+    componentWillMount = () => {
+        
+    };
+
+    render() {
+        return (
+                <MuiThemeProvider>
+                    <HashRouter history={ createHashHistory({ queryKey: false }) }>
+                        <Switch>
+                            <PrivateRoute exact path='/' component={ AppMenu } isAuthenticated={ this.checkAuthentication }/>
+                            <PrivateRoute exact path='/home/:projectName' component={ AppMenu } isAuthenticated={ this.checkAuthentication }/>
+                            <Redirect from ='/home/:projectName' to='/home' component={ AppMenu } isAuthenticated={ this.checkAuthentication }/>
+                            <PrivateRoute exact path='/feedback' component={ FeedbackForm } isAuthenticated={ this.checkAuthentication }/>
+                            <PrivateRoute exact path='/form-history' component={ FormHistory } isAuthenticated={ this.checkAuthentication }/>
+                            <PrivateRoute path="/feedback/:formId" component={ FormDetail } isAuthenticated={ this.checkAuthentication }/>
+                            <Route exact path='/login' component={ LoginForm }/>
+                        </Switch>
+                    </HashRouter>
+                </MuiThemeProvider>
+        );
+    }
+
+    checkAuthentication = () => Object.keys(this.props.store.getState().jwt).length > 0;
+}
 
 App.PropTypes = {
     store: PropTypes.object.isRequired,
