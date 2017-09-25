785e0cb0a4c5942861f0ac7e15ba530a10468930
stehrenberg <sehrenbe@hm.edu>
2017-09-25 15:59:15 +0200

    Use multiline TextField from MaterialUI as TextInput

--- src/components/answers/TextInput.js
+++ src/components/answers/TextInput.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
 
 const TextInput = ({isReadOnly, handleChange, value, name, inputType, placeholder}) => {
 
     const optionalProps = isReadOnly? { disabled: true } : {};
 
-    return <textarea
+    return <TextField
         name={ name }
         type={ inputType }
         value={ value }
-        placeholder={ placeholder }
+        hintText={ placeholder }
+        multiLine={ true }
         onChange={ (event) => handleChange(name, event.target.value) }
-        rows={ 3 }
+        rows={ 1 }
+        fullWidth={ true }
         {...optionalProps}/>;
 };
 
