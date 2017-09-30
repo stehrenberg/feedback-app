3f05c854f09c7528c26d6523b094b2aa59de8c5f
stehrenberg <sehrenbe@hm.edu>
2017-09-30 15:48:20 +0200

    Add surveyId as PropType, minor refactoring

--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -4,19 +4,18 @@ import { connect } from 'react-redux';
 import TodoItem from './TodoItem';
 import { List } from 'material-ui/List';
 
-const TodoList = ({ todos, surveyId }) => {
+const TodoList = ({ surveyId, todos }) => {
     return (
         <List key={ todos }>
             { todos.map(
-                (todo) => <TodoItem key={ todo.text }
-                                    text={ todo.text }
-                                    completed={ todo.completed } {...todo}/>) }
+                (todo) => <TodoItem key={ todo.text } {...todo}/>) }
         </List>
     )
 };
 
 TodoList.PropTypes = {
-    todos: PropTypes.arrayOf(PropTypes.string),
+    todos: PropTypes.arrayOf(PropTypes.string).isRequired,
+    surveyId: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state, ownProps) => {
