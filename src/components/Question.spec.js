630e3ab9ea1886fc8429dabd0489198bcf8022b4
stehrenberg <sehrenbe@hm.edu>
2017-10-02 20:31:11 +0200

    Fix test

--- src/components/Question.spec.js
+++ src/components/Question.spec.js
@@ -4,7 +4,18 @@ import StorageMock from '../util/StorageMock';
 import Question from './Question';
 
 it('renders without crashing', () => {
+
+    const dummyProps = {
+        name: 'bla',
+        surveyId: '20171010-testproject',
+        inputType: 'number',
+        isReadOnly: 'false',
+        onChange: () => {},
+        value: '',
+        placeholder: 'bla',
+    };
+
     window.localStorage = StorageMock();
     const div = document.createElement('div');
-    ReactDOM.render(<Question />, div);
+    ReactDOM.render(<Question {...dummyProps}/>, div);
 });
