00f56f327c05fb111fa01d35073bc128ddc0190b
stehrenberg <sehrenbe@hm.edu>
2017-10-02 18:52:10 +0200

    Update creation of surveyId to include projectname as suffix and use Redux within FeedbackForm

--- src/pages/feedback-form/FeedbackForm.js
+++ src/pages/feedback-form/FeedbackForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Moment from 'moment';
 import RaisedButton from 'material-ui/RaisedButton';
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import { connect } from 'react-redux';
 
 import Questionnaire from '../../components/Questionnaire';
 import LogoHeader from '../../components/LogoHeader';
@@ -10,28 +11,27 @@ import questions from '../../config/questionTexts.json';
 import '../../app.css'
 
 const FeedbackForm = (props) => {
-        return (
-            <div className="FeedbackForm">
-                <LogoHeader title="Cooperation Feedback Questionnaire"/>
-                <div className="App-content">
-                    <Questionnaire id={ generateId() }
-                                   questions={ questions.questionTexts }
-                                   todos={ [] }
-                                   isReadOnly={ false }/>
-                    <div className="App-footer">
-                        <RaisedButton className="nav-btn"
-                                      onClick={ props.history.goBack }
-                                      icon={ <ArrowBack /> }
-                                      primary={ true }
-                                      label={ "Back" }/>
-                    </div>
+    console.log(props.id);
+    return (
+        <div className="FeedbackForm">
+            <LogoHeader title="Cooperation Feedback Questionnaire"/>
+            <div className="App-content">
+                <Questionnaire id={ props.id }
+                               questions={ questions.questionTexts }
+                               todos={ [] }
+                               isReadOnly={ false }/>
+                <div className="App-footer">
+                    <RaisedButton className="nav-btn"
+                                  onClick={ props.history.goBack }
+                                  icon={ <ArrowBack /> }
+                                  primary={ true }
+                                  label={ "Back" }/>
                 </div>
             </div>
-        );
-
-
+        </div>
+    );
 };
 
-const generateId = () => Moment().format("YYYYMMDD");
+const mapStateToProps = (state, ownProps) => ({ id: `${ Moment().format("YYYYMMDD") }-${ state.projectName }` });
 
-export default FeedbackForm;
+export default connect(mapStateToProps)(FeedbackForm);
